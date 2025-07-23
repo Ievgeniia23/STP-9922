d201ad23a98017bc8f826ac4f9c80a0095ad2ee4
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2025-07-23 07:01:23 +0200

    Corrections

--- src/js/advantages-swiper.js
+++ src/js/advantages-swiper.js
@@ -1,89 +1,36 @@
 import Swiper from 'swiper';
-import { Pagination } from 'swiper/modules';
-
-// import { Navigation, Keyboard } from 'swiper/modules';
-
+import { Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-
-const swiper = new Swiper('.swiper', {
-  modules: [Pagination],
-  slidesPerView: 3,
-
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-       slidesPerView: 2,
-      spaceBetween: 12,
-    },
-    1200: {
-      slidesPerView: 3,
-      spaceBetween: 24,
-    },
-  },
+const swiperEl = document.querySelector('[data-swiper="advantages"]');
+const paginationEl = document.querySelector('[data-pagination="advantages"]');
+const prevBtn = document.querySelector(
+  '[data-swiper-button-prev="advantages"]'
+);
+const nextBtn = document.querySelector(
+  '[data-swiper-button-next="advantages"]'
 );
 
-//   modules: [Navigation, Pagination],
-// direction: 'vertical',
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-// });
-
-
-
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   const projSwiper = new Swiper('.projects-swiper', {
-//     modules: [Navigation, Keyboard],
-//     slidesPerView: 1,
-//     spaceBetween: 20,
-//     grabCursor: true,
-//     allowTouchMove: true,
-
-//     keyboard: {
-//       enabled: true,
-//       onlyInViewport: true,
-//     },
-//     //  mousewheel: {
-//     //    invert: false,
-//     //  },
-//   });
-//   const buttonProjPrev = document.querySelector('.proj-left-button');
-//   const buttonProjNext = document.querySelector('.proj-right-button');
-
-//   function updateButtons(projSwiper, buttonProjPrev, buttonProjNext) {
-//     if (projSwiper.isBeginning) {
-//       buttonProjPrev.disabled = true;
-//     } else {
-//       buttonProjPrev.disabled = false;
-//     }
-
-//     if (projSwiper.isEnd) {
-//       buttonProjNext.disabled = true;
-//     } else {
-//       buttonProjNext.disabled = false;
-//     }
-//   }
-
-//   buttonProjPrev.addEventListener('click', () => {
-//     projSwiper.slidePrev();
-//     updateButtons(projSwiper, buttonProjPrev, buttonProjNext);
-//     buttonProjPrev.blur();
-//   });
-
-//   buttonProjNext.addEventListener('click', () => {
-//     projSwiper.slideNext();
-//     updateButtons(projSwiper, buttonProjPrev, buttonProjNext);
-//     buttonProjNext.blur();
-//   });
-
-//   updateButtons(projSwiper, buttonProjPrev, buttonProjNext);
-// });
+if (swiperEl && paginationEl) {
+  const swiper = new Swiper(swiperEl, {
+    modules: [Pagination, Navigation],
+    pagination: {
+      el: paginationEl,
+      clickable: true,
+    },
+    navigation: {
+      prevEl: prevBtn,
+      nextEl: nextBtn,
+    },
+    slidesPerView: 2,
+    spaceBetween: 12,
+    breakpoints: {
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+  });
+}
